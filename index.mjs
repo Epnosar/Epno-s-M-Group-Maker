0417a0fd5984dc483bfba7a0a771643edabce40e
Pearson Federico <Pearsonf12@gmail.com>
2026-02-14 20:59:52 -0800

    Enable persistent storage

--- index.mjs
+++ index.mjs
@@ -371,7 +371,7 @@ const commands = [
 async function registerCommands() {
   const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
   await rest.put(
-    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+    Routes.applicationCommands(process.env.CLIENT_ID),
     { body: commands }
   );
 }
